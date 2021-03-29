092d21726b382f9160b7701ce1cb422fb9feb6aa
Raja Ganapathy <79167443+rajaganapathy2000@users.noreply.github.com>
2021-03-29 18:44:17 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,11 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
-                    InlineKeyboardButton("About Me", callback_data="about_data")
+                    InlineKeyboardButton("⭕️ JOIN OUR GROUP ⭕️", url="https://t.me/pdfworld"),
+                    InlineKeyboardButton("⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/tamilbooksworld")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("⭕️ JOIN OUR BACKUP CHANNEL ⭕️", url="https://t.me/noolagamlibrary")
                 ]
             ]
         )
@@ -70,7 +69,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "⭕️ OUR GROUP ⭕️", url="https://t.me/pdfworld")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
